e3c1601bcb475ec0203e3b7e4dde82a26b7fe29e
yuanchicheng <115764976+yuanchicheng@users.noreply.github.com>
2026-05-13 10:35:15 +0800

    Update week09-6.cpp

--- week09/week09-6.cpp
+++ week09/week09-6.cpp
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main()
+{
+    // 陣列初始化
+    int a[10] = {11, 22, 33, 44, 55, 66, 77, 88, 97, 99};
+
+    for (int i = 0; i < 10; i++)
+        printf("%d ", a[i]);   // 印出陣列
+
+    printf("\n");
+
+    for (int k = 0; k < 10 - 1; k++) {   // Bubble Sort
+        for (int i = 0; i < 10 - 1; i++) {   // 兩兩比較
+            if (a[i] < a[i + 1]) {   // 前面小於後面
+                int temp = a[i];     // 暫存
+                a[i] = a[i + 1];
+                a[i + 1] = temp;
+            }
+        }
+    }
+
+    // 排序結果
+    for (int i = 0; i < 10; i++)
+        printf("%d ", a[i]);   // 印出陣列
+
+    printf("\n");   // 換行
+}
